0f23f4d5671e9878f00b6f0f04f5adfd1dcacf6a
yumble <hnjn1101@gmail.com>
2023-08-10 21:22:33 +0900

    feat: level2 문제 풀이 정렬 및 순환 큐

---
+++ level2/backjun11650.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+data_list = [(a, b) for a, b in [map(int, input().split()) for _ in range(n)]]
+
+data_list = sorted(data_list, key=lambda x:(x[0], x[1]))
+
+for data in data_list:
+    print(f'{data[0]} {data[1]}')
